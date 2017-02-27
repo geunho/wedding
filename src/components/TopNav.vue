ee8274e93ca5b3e238188104d914a9011cf7f830
GeunhoKim <geunho.khim@gmail.com>
2017-02-28 00:14:59 +0900

    4

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,21 +1,15 @@
 <template>
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
-    <div class="navbar-header">
-      <!--button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button-->
-      <a class="navbar-brand" :href="'#/invite'">05<br>14</a>
-    </div>
-    <div class="nav-content">
+    <!--div class="navbar-header">
+    </div-->
+    <div>
       <ul class="nav navbar-nav navbar-left">
-       <li><a :href="'#/invite'">초대합니다.</a></li>
-       <li><a :href="'#/aboutus'">근호 & 미경</a></li>
-       <li><a :href="'#/photos'">사진첩</a></li>
-       <li><a :href="'#/location'">오시는 길</a></li>
-       <li><a :href="'#/attendance'">참석 여부</a></li>
+       <li><a class="navbar-brand" :href="'#/invite'">05<br>14</a></li>
+       <li><a class="navbar-list" :href="'#/aboutus'">근호&미경</a></li>
+       <li><a class="navbar-list" :href="'#/photos'">사진</a></li>
+       <li><a class="navbar-list" :href="'#/location'">장소</a></li>
+       <li><a class="navbar-list" :href="'#/attendance'">참석</a></li>
      </ul>
     </div>
   </div>
@@ -30,23 +24,39 @@
   min-height: 50px
   margin-bottom: 20px
   border-color: $border-color
-  background-image: url('../assets/cherry-blossom.png')
+  /*background-image: url('../assets/cherry-blossom.png')
   background-repeat: no-repeat
   background-position: right bottom
-  background-size: 70%
+  background-size: 70%*/
 }
 
 .navbar-nav {
-  margin-top: 20px
+  margin-top: 10px
 }
 
-.nav > li > a {
+.nav>li>a>.navbar-list {
   letter-spacing: 1.15px
-  font-size: 17px
+  font-size: 15px
 }
 
 .navbar-default {
   background-color: #fff
+}
+
+.navbar-collapse.collapse {
+  display: block!important;
+}
+
+.navbar-nav>li, .navbar-nav {
+  float: left !important;
+}
+
+.navbar-nav.navbar-right:last-child {
+  margin-right: -15px !important;
+}
+
+.navbar-right {
+  float: right!important;
 }
 
 </style>
