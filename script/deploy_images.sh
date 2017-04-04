4bb0031752ea2a3aa7906e285619364e59a8e0e5
Geunho Khim <gkhim@ebay.com>
2017-04-04 12:32:35 +0900

    수정본 사진으로 변경

--- script/deploy_images.sh
+++ script/deploy_images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -rave "ssh -i ~/Security/aws-keypair-imac.pem" src/assets/images/IMG_* ubuntu@ec2-13-124-28-33.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/app/images/ --delete
-rsync -rave "ssh -i ~/Security/aws-keypair-imac.pem" src/assets/images/original/IMG_* ubuntu@ec2-13-124-28-33.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/app/images/original/ --delete
+rsync -rave "ssh -i ~/Security/aws-keypair-imac.pem" --delete  src/assets/images/IMG_* ubuntu@ec2-13-124-28-33.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/app/images/
+rsync -rave "ssh -i ~/Security/aws-keypair-imac.pem" --delete src/assets/images/original/IMG_* ubuntu@ec2-13-124-28-33.ap-northeast-2.compute.amazonaws.com:/home/ubuntu/app/images/original/
